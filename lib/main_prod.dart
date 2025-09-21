0569900d349a85650edbb0d2b7b8040378179ea1
mosfeqanik <mosfeqanik01@gmail.com>
2025-09-21 11:58:38 +0600

    refactor: rename development flavor and update launch configuration

--- lib/main_prod.dart
+++
@@ -1,9 +0,0 @@
-import 'flavors/flavor_config.dart';
-import 'main_common.dart';
-
-void main() {
-  mainCommon(
-      flavor: Flavor.PRODUCTION,
-      apiBaseUrl: "https://api.production.example.com",
-      appName: "MyApp Production");
-}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,8 +9,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_flavors_test/flutter_app.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_flavors_test/main_common.dart';
-
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
